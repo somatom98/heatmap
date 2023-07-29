3e09ac3e31fd1b3c4d7f16936702374ab4520481
Tommaso Lauria <tommasolauria98@gmail.com>
2023-07-29 18:12:57 +0200

    chore: add currencies

--- repositories/currency_repository.go
+++ repositories/currency_repository.go
@@ -17,7 +17,7 @@ func (c *CurrencyRepository) GetAll() []models.Currency {
 			Name:            "Bitcoin",
 			Price:           30000,
 			Last24Variation: 12.5,
-			MarketCap:       1000000000,
+			MarketCap:       100000000,
 		},
 		{
 			Name:            "Ethereum",
@@ -30,6 +30,78 @@ func (c *CurrencyRepository) GetAll() []models.Currency {
 			Price:           1.5,
 			Last24Variation: 5,
 			MarketCap:       1000000,
+		},
+		{
+			Name:            "Litecoin",
+			Price:           100,
+			Last24Variation: 0,
+			MarketCap:       100000,
+		},
+		{
+			Name:            "Monero",
+			Price:           50,
+			Last24Variation: 0,
+			MarketCap:       10000,
+		},
+		{
+			Name:            "Dogecoin",
+			Price:           0.5,
+			Last24Variation: 0,
+			MarketCap:       1000,
+		},
+		{
+			Name:            "Dash",
+			Price:           100,
+			Last24Variation: 0,
+			MarketCap:       100,
+		},
+		{
+			Name:            "Zcash",
+			Price:           1000,
+			Last24Variation: 0,
+			MarketCap:       10,
+		},
+		{
+			Name:            "Tether",
+			Price:           1,
+			Last24Variation: 0,
+			MarketCap:       1,
+		},
+		{
+			Name:            "Bitcoin Cash",
+			Price:           100,
+			Last24Variation: 0,
+			MarketCap:       1,
+		},
+		{
+			Name:            "Bitcoin SV",
+			Price:           100,
+			Last24Variation: 0,
+			MarketCap:       1,
+		},
+		{
+			Name:            "Binance Coin",
+			Price:           100,
+			Last24Variation: 0,
+			MarketCap:       1,
+		},
+		{
+			Name:            "EOS",
+			Price:           100,
+			Last24Variation: 0,
+			MarketCap:       1,
+		},
+		{
+			Name:            "Tezos",
+			Price:           100,
+			Last24Variation: 0,
+			MarketCap:       1,
+		},
+		{
+			Name:            "Cardano",
+			Price:           100,
+			Last24Variation: 0,
+			MarketCap:       1,
 		},
 	}
 }
